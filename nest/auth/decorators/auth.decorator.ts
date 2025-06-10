a0c529adda30a8c2bc8c3857da7bc6a8fdd615e6
krylov <vaness.engineer@gmail.com>
2025-06-10 11:55:21 +0300

    chore: remove example folder

--- nest/auth/decorators/auth.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { applyDecorators, UseGuards } from '@nestjs/common'
-import { JwtAuthGuard } from '../guards/jwt.guard'
-
-export const Auth = () =>
-	applyDecorators(
-		UseGuards(JwtAuthGuard)
-	)
--- nest/playlist/dto/toggle-video.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator'
-
-export class ToggleVideoDto {
-	@IsString()
-	@IsNotEmpty()
-	videoId: string
-}
